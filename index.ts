7171306cada95f2245500ef081c492027a3d479e
zsofia boldizsar <zsofia.boldizsar@hu.ibm.com>
2021-06-27 16:27:01 +0200

    calculateDueDate func

--- index.ts
+++ index.ts
@@ -1,3 +1,23 @@
-export const calculateDueDate = (submitDate, turnaroundTime): string => {
- return 'x';
+import { isWorkingHours } from './src/util';
+
+export const calculateDueDate = (submissionDate: Date, turnaroundTime: any): Date | Error => {
+  if (!isWorkingHours(submissionDate)) {
+    return new Error('Please submit your issue during working hours');
+  }
+  if (isNaN(turnaroundTime)) {
+    return new Error('Please provide a number for the turnaround time');
+  }
+
+  let dueDate = new Date(submissionDate.getTime());
+  let elapsedHours = 0;
+  while (elapsedHours < turnaroundTime) {
+    dueDate = new Date(dueDate.setHours(dueDate.getHours() +1));
+    if (isWorkingHours(dueDate)) {
+      elapsedHours++;
+    }
+  }
+
+  return dueDate;
 };
+
+// console.log('Due date:', calculateDueDate(new Date('2021-06-25 17:00:00'), 16));
--- tests/index.spec.ts
+++ tests/index.spec.ts
@@ -1,8 +1,45 @@
 import { assert } from 'chai';
+import { calculateDueDate } from '../index';
 
-describe('calculateDueDate()', () => {
-  it('happy path - should return with string represantation of due date', () => {});
-  it('should throw error if submit date falls on weekend', () => {});
-  it('should throw error if turnaround time is not a number', () => {});
-  it('should throw error if submit date is not a valid date string', () => {});
+describe.only('calculateDueDate()', () => {
+
+  const FAKE_DATE = new Date('2021-06-29 16:01:08');
+  const turnaroundTime = 16;
+
+  it('happy path - should return with due date', () => {
+    const expectedResult = new Date('2021-07-01 16:01:08');
+    const acutalResult = calculateDueDate(FAKE_DATE, turnaroundTime);
+
+    assert.deepEqual(acutalResult, expectedResult);
+  });
+
+  it('should return error message if submission date is outside of working hours', () => {
+    const weekendSubmissionDate = new Date('2021-06-27 16:01:08');
+    let result = null;
+    let expectedError: Error = null;
+    try {
+      result = calculateDueDate(weekendSubmissionDate, turnaroundTime);
+    } catch (error) {
+      expectedError = error;
+    }
+
+    assert.isNull(result);
+    /* assert.isNotNull(expectedError);
+    assert.isTrue(expectedError.message.startsWith('Please submit your issue during working hours')); */
+
+  });
+  /* it('should throw error if turnaround time is not a number', () => {
+    let result = null;
+    let expectedError: Error = null;
+    try {
+      result = calculateDueDate(submissiontDate, '16');
+    } catch (error) {
+      expectedError = error;
+    }
+
+    assert.isNull(result);
+    assert.isNotNull(expectedError);
+    assert.isTrue(expectedError.message.startsWith('Please provide a number for the turnaround time.'));
+
+  }); */
 });
