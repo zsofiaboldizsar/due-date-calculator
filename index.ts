2dff6674e75e64a32e18dc8628415255f59d3474
zsofia boldizsar <zsofia.boldizsar@hu.ibm.com>
2021-06-27 17:00:53 +0200

    throw error, fix turnaroundTime type

--- index.ts
+++ index.ts
@@ -1,11 +1,11 @@
 import { isWorkingHours } from './src/util';
 
-export const calculateDueDate = (submissionDate: Date, turnaroundTime: any): Date | Error => {
+export const calculateDueDate = (submissionDate: Date, turnaroundTime: number): Date | Error => {
   if (!isWorkingHours(submissionDate)) {
-    return new Error('Please submit your issue during working hours');
+    throw new Error('Please submit your issue during working hours');
   }
   if (isNaN(turnaroundTime)) {
-    return new Error('Please provide a number for the turnaround time');
+    throw new Error('Please provide a number for the turnaround time');
   }
 
   let dueDate = new Date(submissionDate.getTime());
@@ -20,4 +20,4 @@ export const calculateDueDate = (submissionDate: Date, turnaroundTime: any): Dat
   return dueDate;
 };
 
-// console.log('Due date:', calculateDueDate(new Date('2021-06-25 17:00:00'), 16));
+// console.log('Due date:', calculateDueDate(new Date('2021-06-25 16:00:00'), 16));
